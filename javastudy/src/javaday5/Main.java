5cb2b9de049cc469e6fceb72a547a1448cdfa80f
parkyonkun <redaygun@gmail.com>
2018-03-29 15:59:04 +0900

    cirlce mian 적용

--- javastudy/src/javaday5/Main.java
+++ javastudy/src/javaday5/Main.java
@@ -1,10 +1,15 @@
 package javaday5;
 
+import javaday5.shape.Circle;
+
 public class Main {
-
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Circle c1 = new Circle();
+		c1.printCircle();
+		c1.moveCenter(10, 10);
+		c1.printCircle();
+		c1.resize(10.1);
+		c1.printCircle();
 	}
-
 }
