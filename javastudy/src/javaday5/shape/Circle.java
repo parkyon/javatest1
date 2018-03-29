84cd74887922cc089748240e520f64f54a9d5da1
parkyonkun <redaygun@gmail.com>
2018-03-29 15:18:35 +0900

    javaday5.shape에 circle클래스 및 get set 추가

---
+++ javastudy/src/javaday5/shape/Circle.java
@@ -0,0 +1,26 @@
+package javaday5.shape;
+
+public class Circle {
+		private int centerX;
+		private int centerY;
+		private double radius;
+		public int getCenterX() {
+			return centerX;
+		}
+		public void setCenterX(int centerX) {
+			this.centerX = centerX;
+		}
+		public int getCenterY() {
+			return centerY;
+		}
+		public void setCenterY(int centerY) {
+			this.centerY = centerY;
+		}
+		public double getRadius() {
+			return radius;
+		}
+		public void setRadius(double radius) {
+			this.radius = radius;
+		}
+	
+}
