681b4cdfb77ec7e004913c203fbfbef5ab58af6d
parkyonkun <redaygun@gmail.com>
2018-03-29 17:25:00 +0900

    Rect 클래 객체 생성 및 테스트(java5 shape)

--- javastudy/src/javaday5/shape/Main.java
+++ javastudy/src/javaday5/shape/Main.java
@@ -1,6 +1,4 @@
-package javaday5;
-
-import javaday5.shape.Circle;
+package javaday5.shape;
 
 public class Main {
 	
@@ -11,5 +9,13 @@ public class Main {
 		c1.printCircle();
 		c1.resize(10.1);
 		c1.printCircle();
+		
+		Rect r = new Rect();
+		r.print();
+		Rect r2 = new Rect(0,0,10, -10);
+		r2.print();
+		Rect r3 = new Rect(new Point(0,0), 
+					new Point(10, -10));
+		r3.print();
 	}
 }
