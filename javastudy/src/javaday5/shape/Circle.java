dd9361d678e3801a8cc82146b77e5fc298b5abc6
parkyonkun <redaygun@gmail.com>
2018-03-29 15:43:37 +0900

    circle 클래스에 생성자 추

--- javastudy/src/javaday5/shape/Circle.java
+++ javastudy/src/javaday5/shape/Circle.java
@@ -22,5 +22,21 @@ public class Circle {
 		public void setRadius(double radius) {
 			this.radius = radius;
 		}
-	
+
+		public Circle() {
+			this.setCenter(0, 0);
+			this.setRadius(5.0);
+		}
+		public Circle(int x, int y, double r) {
+			this.setCenter(x,y);  //xy ют╥б
+			this.setRadius(10.0);
+		}
+		public Circle(Circle c) {
+			this.setCenter(c.centerX, c.centerY);
+			this.setRadius(c.radius);
+		}
+		public void setCenter(int x, int y) {
+			this.setCenterX(x);
+			this.setCenterY(y);
+		}
 }
