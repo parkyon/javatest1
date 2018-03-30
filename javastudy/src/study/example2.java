0ea6213c3b1bde497823be10562063ea9950e378
parkyonkun <redaygun@gmail.com>
2018-03-30 11:45:44 +0900

    생성자 오버라이딩 연습을 위한 예제 및 main

---
+++ javastudy/src/study/example2.java
@@ -0,0 +1,12 @@
+package study;
+
+
+public class example2 {
+	public static void main(String[] args) {
+		System.out.println(number.sum(10, 10));
+		System.out.println(number.sum(10, 10.0));
+		System.out.println(number.sum(10.0, 10));
+		Math.sqrt(1);
+		
+	}
+}
---
+++ javastudy/src/study/number.java
@@ -0,0 +1,21 @@
+package study;
+
+public class number {
+	int num;
+	public static int sum(int num1, int num2) {
+		return num1 + num2;
+		
+	
+	}
+	public static double sum(double num1, int num2) {
+		return num1 + num2;
+		
+	
+	}
+	public static double sum(int num1, double num2) {
+		return num1 + num2;
+	}
+	public static double sum(double num1, double num2) {
+		return num1 + num2;
+	}
+}
